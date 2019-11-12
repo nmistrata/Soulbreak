7ef376c1cbb3cf5ab9221f93a0921ae284fa1d0f
Zhou <mz25@illinois.edu>
2019-11-12 13:16:59 -0600

    did stuff with projectiles and spels

---
+++ Assets/discusoModifier.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class discusoModifier : MonoBehaviour {
+    int frameCounter;
+    Rigidbody pb;
+    System.Random r;
+	// Use this for initialization
+	void Start () {
+        pb = GetComponent<Rigidbody>();
+        r = new System.Random();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(frameCounter % 100 == 0)
+        {
+            float v = pb.velocity.magnitude;
+            float rx = (float)r.NextDouble()*5;
+            float ry = (float)r.NextDouble()*5;
+            float rz = (float)r.NextDouble()*5;
+            pb.velocity = new Vector3(rx, ry, rz) * v;
+            pb.angularVelocity = new Vector3(rx, ry, rz) *10;
+        }
+        if(frameCounter > 300)
+        {
+            frameCounter = 0;
+        }
+        frameCounter++;
+	}
+}
---
+++ Assets/energyLargoExplosion.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class energyLargoExplosion : MonoBehaviour {
+    public GameObject proj;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject tempProj;
+        GameObject tempProj1;
+        GameObject tempProj2;
+        GameObject tempProj3;
+        GameObject tempProj4;
+        GameObject tempProj5;
+
+        tempProj = Instantiate(proj) as GameObject;
+        tempProj1 = Instantiate(proj) as GameObject;
+        tempProj2 = Instantiate(proj) as GameObject;
+        tempProj3 = Instantiate(proj) as GameObject;
+        tempProj4 = Instantiate(proj) as GameObject;
+        tempProj5 = Instantiate(proj) as GameObject;
+
+        tempProj.transform.position = transform.position;
+        tempProj1.transform.position = transform.position;
+        tempProj2.transform.position = transform.position;
+        tempProj3.transform.position = transform.position;
+        tempProj4.transform.position = transform.position;
+        tempProj5.transform.position = transform.position;
+
+        Rigidbody p = tempProj.GetComponent<Rigidbody>();
+        Rigidbody p1 = tempProj1.GetComponent<Rigidbody>();
+        Rigidbody p2 = tempProj2.GetComponent<Rigidbody>();
+        Rigidbody p3 = tempProj3.GetComponent<Rigidbody>();
+        Rigidbody p4 = tempProj4.GetComponent<Rigidbody>();
+        Rigidbody p5 = tempProj5.GetComponent<Rigidbody>();
+
+        p.velocity = new Vector3(0.5f, 0.5f, 0) * 6;
+        p1.velocity = new Vector3(0.5f, 0, 0.5f) * 6;
+        p2.velocity = new Vector3(0, 0.5f, 0) * 6;
+        p3.velocity = new Vector3(0.5f, 0, 0) * 6;
+        p4.velocity = new Vector3(0,0 , 0.5f) * 6;
+        p5.velocity = new Vector3(0, 0.5f, 0.5f) * 6;
+        Destroy(gameObject);
+    }
+}
