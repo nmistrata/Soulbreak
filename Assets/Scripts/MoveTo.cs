ae68026590bc73073e7ab4b8cc6ffdbcb97c3383
bepzi <dev.ben01189998819991197253@gmail.com>
2019-11-13 15:00:35 -0600

    Replace NavMeshes with naive movement

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+    public Transform player;
+
+    public float velocity = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+        transform.position += transform.forward * Time.deltaTime * velocity;
+	}
+}
