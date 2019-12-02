574d29d0ff40e1a129e4e0fd4e76802840c9eb0c
bepzi <dev.ben01189998819991197253@gmail.com>
2019-12-02 15:19:20 -0600

    Rename script

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public Transform player;
+
+    public float velocity = 2f;
+
+    public float maxHealth = 1f;
+    private float health;
+
+    public float damage = 1f;
+
+	void Start () {
+        health = maxHealth;
+	}
+	
+	void Update () {
+        if (health <= 0.0f) {
+            // TODO: Play a sound effect, etc.
+            gameObject.SetActive(false);
+        }
+
+
+        if (Mathf.Abs(Vector3.Distance(transform.position, player.position)) > 0.5) {
+            transform.LookAt(player);
+            transform.position += transform.forward * Time.deltaTime * velocity;
+        } else {
+            // Too close to player, stop moving and hit them
+            Debug.Log("Hitting the player!");
+            velocity = 0;
+        }
+	}
+}
