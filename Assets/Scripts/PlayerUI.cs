a2f71b7f9b6cab45df7b01673bcab8fa663f2349
Mistrata <nm7@illinois.edu>
2019-12-10 18:48:48 -0600

    create player ui

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+    public RectTransform healthBar;
+    public Text wandLevelText;
+    public Text curFloorText;
+    private const float HEALTHBAR_LENGTH = 680f;
+
+    private void Awake()
+    {
+        GameManager.playerUI = gameObject;
+    }
+    private void Update()
+    {
+        Player p = GameManager.player.GetComponent<Player>();
+        healthBar.offsetMax = new Vector2(-HEALTHBAR_LENGTH * (1 - p.health/p.maxHealth), healthBar.offsetMax.y);
+
+        wandLevelText.text = "Wand Level " + p.getCurrentWand().GetLevel();
+
+        curFloorText.text = "Floor " + GameManager.dungeon.curLevel;
+    }
+}
